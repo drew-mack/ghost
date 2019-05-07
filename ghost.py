c2c714cd6e04a45b760e3cfba37124583f425715
drew.mack <realdrewmack@gmail.com>
2019-05-07 18:13:35 -0500

    barebones player win/loss logic based on whether the word is on the list. loading the tree full of words is working as intended

--- ghost.py
+++ ghost.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
+import time
+
 class TreeNode:
     def __init__(self, letter):
         self.letter = letter
         self.children = dict()
         self.word_terminator = False
 
+    def __repr__(self):
+        return self.letter
+
 
 class Player:
     def __init__(self, number):
         self.number = number
         self.strikes = 0
+
+    def __repr__(self):
+        return f'player {self.number}'
 
 
 class Ghoster:
@@ -20,33 +28,40 @@ class Ghoster:
     def add_word(self, word):
         current_node = self.word_tree
         for letter in word:
-            has_child = False
-            for child in current_node.children.keys():
-                if child = letter:
-                    current_node = current_node.children[child]
-                    has_child = True
-                    break
-            if not has_child:
+            if letter in current_node.children.keys():
+                current_node = current_node.children[letter]
+                continue
+            else:
                 new_node = TreeNode(letter)
                 current_node.children[letter] = new_node
                 current_node = new_node
         current_node.word_terminator = True
 
-    def string_valid(self, string):
-        current_node = self.word_tree # womb
+    def string_valid(self, string, player):
+        current_node = self.word_tree
+        buffer = 0
         for letter in string:
-            if current_node.word_terminator and len(string) > 3:
-                return False
+            buffer += 1
             if letter in current_node.children.keys():
                 current_node = current_node.children[letter]
-                continue
+                print(current_node)
             else:
+                print(f'{string} does not begin a real word.')
                 return False
+            if current_node.word_terminator and buffer > 3:
+                print(f'{string} is a word. {player} loses.')
+                return False
+        print('keep playing')
         return True
 
-
     def word_prep(self, file_path):
-        with open(filepath, 'r') as f:
-
+        with open(file_path, 'r') as f:
+            for line in f:
+                self.add_word(line.strip())
 
 if __name__ == '__main__':
+    p1 = Player(1)
+    p2 = Player(2)
+    g = Ghoster()
+    g.word_prep('wordlist_kevin_atkinson.txt')
+    print(g.string_valid('anything', p1))
