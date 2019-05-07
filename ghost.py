babdd42ddc06c352d1d499ec9db54c22316d92fc
drew.mack <realdrewmack@gmail.com>
2019-05-06 19:34:54 -0500

    initial commit

---
+++ ghost.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+class TreeNode:
+    def __init__(self, letter):
+        self.letter = letter
+        self.children = list()
+        self.word_terminator = False
+
+class Ghoster:
+    def __init__(self):
+
+
+    def word_prep(self, file_path):
+        with open(filepath, 'r') as f:
+
+
+if __name__ == '__main__':
