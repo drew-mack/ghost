afdb705cdd7ee8380579a1aa62c920d36179494d
drew.mack <realdrewmack@gmail.com>
2019-05-07 19:33:10 -0500

    Robust playable version. Many syle improvements along with many syntactic improvements

--- ghost.py
+++ ghost.py
@@ -33,38 +33,39 @@ class Ghoster:
         players.append(p)
         return players
 
-    def game_loop(self, p1, p2):
+    def game_loop(self):
+        p1 = Player(1)
+        p2 = Player(2)
         game_over = False
         current_str = ''
         if random.random() > 0.5:
             players = [p1, p2]
         else:
             players = [p2, p1]
-        print('Welcome to ghost! To see the rules, open the readme.md included in this repository.\n' \
-              f'{current_player.capitalize()} will begin this round.')
+        print('\n\nWelcome to ghost! To see the rules, open the readme.md included in this repository.\n' \
+              f'{str(players[0]).capitalize()} will begin this round.\n')
         while not game_over:
-            new_letter = get_input(players[0], current_str)
-            print(f'trying {current_str}{new_letter})
+            new_letter = self.get_input(players[0], current_str)
             response_code = self.string_valid(f'{current_str}{new_letter}')
             if response_code == 0:
                 players = self.switch_players(players)
                 current_str = f'{current_str}{new_letter}'
             elif response_code == 1:
                 players[0].strikes += 1
-                print(f'{current_str}{new_letter} is not a valid beginning to a word.')
-                if players[0].strikes = 3:
-                    print(f'Oh no, {players[0]}! That was your 3rd strike!\n' \
+                print(f'\n\n"{current_str.capitalize()}{new_letter}" is not a valid beginning to any word.')
+                if players[0].strikes == 3:
+                    print(f'\n\nOh no, {players[0]}! That was your 3rd strike!\n' \
                           f'That means {players[0]} is the loser, and {players[1]} is the victor!')
                     game_over = True
                 else:
-                    strike_string = f'{players[0].capitalize()}, you now have {players[0].strikes} strike'
+                    strike_string = f'{str(players[0]).capitalize()}, you now have {players[0].strikes} strike'
                     if players[0].strikes == 1:
                         print(f'{strike_string}.')
                     else:
                         print(f'{strike_string}s.')
             elif response_code == 2:
-                print(f'Uh oh, {players[0]}! Since {current_str}{new_letter} is a real word, you lose!\n' \
-                      f'Congratulations on the win, {players[1]}.')
+                print(f'\n\nUh oh, {players[0]}! Since {current_str}{new_letter} is a real word, you lose!\n' \
+                      f'Congratulations on the win, {players[1]}!')
                 game_over = True
         return 0
 
@@ -72,7 +73,7 @@ class Ghoster:
     def get_input(self, player, current_str):
         while 1:
             if len(current_str) > 0:
-                print(f'The current string is {current_str}.')
+                print(f'\nThe current string is {current_str}.')
             string = input(f'Type your letter and press [enter].\n[{player}]> ')
             if len(string) > 1:
                 print('Please type only 1 character, and try again.')
@@ -101,12 +102,9 @@ class Ghoster:
             if letter in current_node.children.keys():
                 current_node = current_node.children[letter]
             else:
-                print(f'{string} does not begin a real word.')
                 return 1
             if current_node.word_terminator and buffer > 3:
-                print(f'{string} is a real, full word.')
                 return 2
-        print('keep playing')
         return 0
 
     def word_prep(self, file_path):
@@ -115,12 +113,10 @@ class Ghoster:
                 self.add_word(line.strip())
 
 if __name__ == '__main__':
-    p1 = Player(1)
-    p2 = Player(2)
     g = Ghoster('wordlist_kevin_atkinson.txt')
     while 1:
         g.game_loop()
-        response = input('Play again? [y/n]\n(All answers not \'y\' or \'Y\' will be counted as no).')
+        response = input('Play again? [y/n]\n(All answers not \'y\' or \'Y\' will be counted as no).\n> ')
         if response in 'yY':
             continue
         else:
